0441544a739399907b4bef3bbf8c8ebec85d4e3d
Diegson Junio <diegson2019@gmail.com>
2022-04-08 12:25:38 -0300

    Implementando o game over

--- jogo.js
+++ jogo.js
@@ -20,7 +20,8 @@ function posicaoRandomica() {
 
         //console.log('elemento selecionado foi: v' + vidas)
         if(vidas > 3) {
-            alert('Interromper o jogo (game ouver)')
+            
+            window.location.href = 'fim_de_jogo.html'
         }
         else {
             document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
